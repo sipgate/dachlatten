ac0595344190ec7c0eb6be5094ef04f4e9ab18be
Jan Seeger <seeger@sipgate.de>
2023-10-09 17:11:05 +0200

    Add sliding window func to dachlatten-flow

---
+++ dachlatten-flow/src/main/java/de/sipgate/dachlatten/flow/Windowed.kt
@@ -0,0 +1,34 @@
+package de.sipgate.dachlatten.flow
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlin.math.max
+import kotlin.math.min
+
+/**
+ * copied from https://github.com/Kotlin/kotlinx.coroutines/pull/2378/files
+ */
+
+fun <T> Flow<T>.windowed(size: Int, step: Int): Flow<List<T>> = flow {
+    // check that size and step are > 0
+    val queue = ArrayDeque<T>(size)
+    val toSkip = max(
+        step - size,
+        0
+    ) // if sbd would like to skip some elements before getting another window, by serving step greater than size, then why not?
+    val toRemove = min(step, size)
+    var skipped = 0
+    collect { element ->
+        if (queue.size < size && skipped == toSkip) {
+            queue.add(element)
+        } else if (queue.size < size && skipped < toSkip) {
+            skipped++
+        }
+
+        if (queue.size == size) {
+            emit(queue.toList())
+            repeat(toRemove) { queue.removeFirst() }
+            skipped = 0
+        }
+    }
+}
---
+++ dachlatten-flow/src/test/java/de/sipgate/dachlatten/flow/WindowedTest.kt
@@ -0,0 +1,46 @@
+package de.sipgate.dachlatten.flow
+
+import app.cash.turbine.test
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class WindowedTest {
+
+    @Test
+    fun testWindowedWithSize2AndStep1() = runTest {
+        flowOf(1, 2, 3, 4).windowed(2, 1).test {
+            assertEquals(listOf(1, 2), awaitItem())
+            assertEquals(listOf(2, 3), awaitItem())
+            assertEquals(listOf(3, 4), awaitItem())
+            awaitComplete()
+        }
+    }
+
+    @Test
+    fun testWindowedWithSize2AndStep2() = runTest {
+        flowOf(1, 2, 3, 4).windowed(2, 2).test {
+            assertEquals(listOf(1, 2), awaitItem())
+            assertEquals(listOf(3, 4), awaitItem())
+            awaitComplete()
+        }
+    }
+
+    @Test
+    fun testWindowedWithSize3AndStep1() = runTest {
+        flowOf(1, 2, 3, 4).windowed(3, 1).test {
+            assertEquals(listOf(1, 2, 3), awaitItem())
+            assertEquals(listOf(2, 3, 4), awaitItem())
+            awaitComplete()
+        }
+    }
+
+    @Test
+    fun testWindowedWithSize3AndStep2() = runTest {
+        flowOf(1, 2, 3, 4).windowed(3, 2).test {
+            assertEquals(listOf(1, 2, 3), awaitItem())
+            awaitComplete()
+        }
+    }
+}
