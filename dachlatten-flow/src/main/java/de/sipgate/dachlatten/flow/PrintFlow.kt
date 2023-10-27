f353ca52251f1591cf05d409e0c8cc6e90ed0076
Jan Seeger <seeger@sipgate.de>
2023-10-27 17:22:23 +0200

    Improve nullability
    Non-null Flows will be passed on as such, while nullable Flows will
    correctly pass through null values.

--- dachlatten-flow/src/main/java/de/sipgate/dachlatten/flow/PrintFlow.kt
+++ dachlatten-flow/src/main/java/de/sipgate/dachlatten/flow/PrintFlow.kt
@@ -4,7 +4,7 @@ import android.util.Log
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
-inline fun <T> Flow<T?>.printFlow(
+inline fun <T> Flow<T>.printFlow(
     tag: String = "from flow",
     crossinline printFunc: (String) -> Unit = { content -> Log.d(tag, content) },
     crossinline stringFunc: (T?) -> String? = { it.toString() }
