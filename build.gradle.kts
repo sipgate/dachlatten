bdc4341948c5bd15f6b3a122249725db682b54a8
Jan Seeger <seeger@sipgate.de>
2023-12-08 12:04:09 +0100

    Clean up Gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,3 @@ plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinAndroid) apply false
 }
-true // Needed to make the Suppress annotation work for the plugins block
