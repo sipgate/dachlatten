679f39a11a3d2269ead456a3f354e896941a79a8
Jan Seeger <seeger@sipgate.de>
2024-05-28 11:06:17 +0200

    Remove unused imports

--- build-logic/android-library/src/main/kotlin/KotlinLibraryUnitTestPlugin.kt
+++ build-logic/android-library/src/main/kotlin/KotlinLibraryUnitTestPlugin.kt
@@ -1,9 +1,7 @@
-import kotlinx.kover.gradle.plugin.dsl.KoverReportExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.api.tasks.testing.Test
-import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.withType
