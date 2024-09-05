e61b9519e6226e27ddd3eecb0dc3414ef5725aab
Jan Seeger <seeger@sipgate.de>
2024-09-05 13:23:29 +0200

    Simplify printFlow

--- dachlatten-flow/src/main/kotlin/de/sipgate/dachlatten/flow/PrintFlow.kt
+++ dachlatten-flow/src/main/kotlin/de/sipgate/dachlatten/flow/PrintFlow.kt
@@ -1,13 +1,12 @@
 package de.sipgate.dachlatten.flow
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 
 inline fun <T> Flow<T>.printFlow(
     tag: String = "from flow",
     crossinline printFunc: (String) -> Unit = { content -> println("$tag: $content") },
     crossinline stringFunc: (T?) -> String? = { it.toString() }
-) = map {
+) = onEach {
     printFunc(stringFunc(it) ?: "null")
-    it
 }
