80810d1045e58cf0c665e4511e0ccbfe74be949b
Jan Seeger <seeger@sipgate.de>
2024-09-05 13:27:33 +0200

    Document WhileSubscribed convenience wrapper

--- dachlatten-flow/src/main/kotlin/de/sipgate/dachlatten/flow/SharingStartedExt.kt
+++ dachlatten-flow/src/main/kotlin/de/sipgate/dachlatten/flow/SharingStartedExt.kt
@@ -2,7 +2,15 @@ package de.sipgate.dachlatten.flow
 
 import kotlinx.coroutines.flow.SharingStarted
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
-fun WhileSubscribed(duration: Duration): SharingStarted {
-    return SharingStarted.WhileSubscribed(stopTimeoutMillis = duration.inWholeMilliseconds)
+/**
+ * This is a convenience wrapper for SharingStarted.WhileSubscribed that takes
+ * Kotlin-time Duration as a parameter and converts it to milliseconds.
+ *
+ * @param stopTimeout configures a delay between the disappearance of the last subscriber and the
+ *                    stopping of the sharing coroutine. It defaults to zero (stop immediately).
+ */
+fun WhileSubscribed(stopTimeout: Duration = 0.seconds): SharingStarted {
+    return SharingStarted.WhileSubscribed(stopTimeoutMillis = stopTimeout.inWholeMilliseconds)
 }
