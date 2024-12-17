15a1bc28b9754543cb47d6e09180ca481f5d5b4a
Jan Seeger <seeger@sipgate.de>
2024-12-17 10:32:57 +0100

    Remove ObjectAnimator

--- dachlatten-datetime-android/src/main/kotlin/de/sipgate/dachlatten/datetime/ObjectAnimatorExt.kt
+++
@@ -1,8 +0,0 @@
-package de.sipgate.dachlatten.datetime
-
-import android.animation.ObjectAnimator
-import kotlin.time.Duration
-
-@Suppress("unused")
-fun ObjectAnimator.setDuration(duration: Duration): ObjectAnimator =
-    setDuration(duration.inWholeMilliseconds)
