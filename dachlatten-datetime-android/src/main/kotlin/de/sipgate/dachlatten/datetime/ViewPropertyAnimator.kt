108efdf43fc41f22b6a01f0acf0c8c55b77df330
Jan Seeger <seeger@sipgate.de>
2024-12-17 13:07:48 +0100

    Add ViewPropertyAnimator.setStartDelay convenience wrapper

--- dachlatten-datetime-android/src/main/kotlin/de/sipgate/dachlatten/datetime/ViewPropertyAnimator.kt
+++ dachlatten-datetime-android/src/main/kotlin/de/sipgate/dachlatten/datetime/ViewPropertyAnimator.kt
@@ -6,3 +6,7 @@ import kotlin.time.Duration
 @Suppress("unused")
 fun ViewPropertyAnimator.setDuration(duration: Duration): ViewPropertyAnimator =
     setDuration(duration.inWholeMilliseconds)
+
+@Suppress("unused")
+fun ViewPropertyAnimator.setStartDelay(duration: Duration): ViewPropertyAnimator =
+    setStartDelay(duration.inWholeMilliseconds)
