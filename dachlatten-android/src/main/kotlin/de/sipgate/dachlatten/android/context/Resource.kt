2586789917385e6f42de9d1907b796ce434fcd22
Jan Seeger <seeger@sipgate.de>
2025-03-26 16:24:31 +0100

    Clean up Resources.getUriToDrawable

--- dachlatten-android/src/main/kotlin/de/sipgate/dachlatten/android/context/Resource.kt
+++ dachlatten-android/src/main/kotlin/de/sipgate/dachlatten/android/context/Resource.kt
@@ -2,24 +2,28 @@ package de.sipgate.dachlatten.android.context
 
 import android.content.ContentResolver
 import android.content.Context
+import android.content.res.Resources
 import android.net.Uri
 import androidx.annotation.AnyRes
-import androidx.core.net.toUri
 
 /**
  * Get Uri referring to Drawable or any other given Resource type
  *
  * Adapted from:https://stackoverflow.com/a/36062748
  *
- * @param context - Context to resolve the Resource against
+ * @receiver - Resource to resolve the Resource Id against
  * @param resourceId - Resource Id to be referenced
  * @return - Uri referencing the given Resource
  */
+fun Resources.getUriToDrawable(
+    @AnyRes resourceId: Int,
+): Uri = Uri.Builder()
+    .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+    .authority(getResourcePackageName(resourceId))
+    .path(getResourceTypeName(resourceId))
+    .appendPath(getResourceEntryName(resourceId))
+    .build()
+
 fun Context.getUriToDrawable(
     @AnyRes resourceId: Int,
-): Uri = (
-    ContentResolver.SCHEME_ANDROID_RESOURCE +
-        "://" + resources.getResourcePackageName(resourceId) +
-        '/' + resources.getResourceTypeName(resourceId) +
-        '/' + resources.getResourceEntryName(resourceId)
-    ).toUri()
+): Uri = resources.getUriToDrawable(resourceId)
