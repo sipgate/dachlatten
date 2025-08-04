f8f131adca7900ea2b2ffd148599164a123f5a41
Jan Seeger <seeger@sipgate.de>
2025-08-04 13:27:13 +0200

    Add compat variant for getParcelableExtra

--- dachlatten-android/src/main/kotlin/de/sipgate/dachlatten/android/intent/IntentExt.kt
+++ dachlatten-android/src/main/kotlin/de/sipgate/dachlatten/android/intent/IntentExt.kt
@@ -2,6 +2,7 @@ package de.sipgate.dachlatten.android.intent
 
 import android.content.Intent
 import android.os.Build
+import android.os.Parcelable
 import java.io.Serializable
 import kotlin.reflect.KClass
 
@@ -11,5 +12,14 @@ fun <T : Serializable> Intent.getSerializableExtraCompat(name: String, clazz: KC
     } else {
         @Suppress("DEPRECATION", "UNCHECKED_CAST")
         getSerializableExtra(name) as? T
+    }
+}
+
+fun <T : Parcelable> Intent.getParcelableExtraCompat(name: String, clazz: KClass<T>): T? {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        getParcelableExtra(name, clazz.java)
+    } else {
+        @Suppress("DEPRECATION", "UNCHECKED_CAST")
+        getParcelableExtra(name) as? T
     }
 }
