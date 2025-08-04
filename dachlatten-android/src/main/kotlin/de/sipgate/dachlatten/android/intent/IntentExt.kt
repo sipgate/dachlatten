a87f7faddb106d379e23eae685efea51153bf2ed
Jan Seeger <seeger@sipgate.de>
2025-08-04 11:42:54 +0200

    Add compat variant for getSerializableExtra

---
+++ dachlatten-android/src/main/kotlin/de/sipgate/dachlatten/android/intent/IntentExt.kt
@@ -0,0 +1,15 @@
+package de.sipgate.dachlatten.android.intent
+
+import android.content.Intent
+import android.os.Build
+import java.io.Serializable
+import kotlin.reflect.KClass
+
+fun <T : Serializable> Intent.getSerializableExtraCompat(name: String, clazz: KClass<T>): T? {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        getSerializableExtra<T>(name, clazz.java)
+    } else {
+        @Suppress("DEPRECATION", "UNCHECKED_CAST")
+        getSerializableExtra(name) as? T
+    }
+}
