9638f7c225abd3bac4ddf20c36dc802312364cd2
Jan Seeger <seeger@sipgate.de>
2025-08-06 10:38:18 +0200

    Convert dachlatten-datetime to JVM lib

--- dachlatten-datetime/build.gradle.kts
+++ dachlatten-datetime/build.gradle.kts
@@ -1,19 +1,10 @@
 plugins {
-    id("android-library-base")
+    id("kotlin-library")
     id("kotlin-library-unit-test")
-    id("android-library-release")
 }
 
 dependencies {
     compileOnly(libs.kotlinx.datetime)
 
     testImplementation(libs.kotlinx.datetime)
-}
-
-android {
-    publishing {
-        singleVariant("release") {
-            withSourcesJar()
-        }
-    }
 }
