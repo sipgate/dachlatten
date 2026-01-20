2994947386a08ecc22e78f847c610b6c1ba37a21
Jan Seeger <seeger@sipgate.de>
2026-01-20 13:21:35 +0100

    Remove unused DEFAULT_LANGUAGE

--- dachlatten-compose/src/main/kotlin/de/sipgate/dachlatten/compose/ContextExt.kt
+++ dachlatten-compose/src/main/kotlin/de/sipgate/dachlatten/compose/ContextExt.kt
@@ -2,8 +2,5 @@ package de.sipgate.dachlatten.compose
 
 import android.content.Context
 
-
-private const val DEFAULT_LANGUAGE = "en"
-
 public val Context.language: String
-    get() = resources.configuration.primaryLocale.language ?: DEFAULT_LANGUAGE
+    get() = resources.configuration.primaryLocale.language
