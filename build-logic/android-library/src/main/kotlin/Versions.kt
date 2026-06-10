0c92764a36477ee77d38d4d9e49cf64f18dd1d1a
Jan Seeger <seeger@sipgate.de>
2026-06-10 08:59:02 +0200

    Bump ByteCode compatibility version to Java 11

--- build-logic/android-library/src/main/kotlin/Versions.kt
+++ build-logic/android-library/src/main/kotlin/Versions.kt
@@ -3,4 +3,4 @@ import org.gradle.api.JavaVersion
 
 internal val KOTLIN_VERSION = KotlinVersion.KOTLIN_2_3
 
-internal val JAVA_VERSION = JavaVersion.VERSION_1_8
+internal val JAVA_VERSION = JavaVersion.VERSION_11
